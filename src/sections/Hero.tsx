dc897fd3ad9d18465de0764ffb32a5417c60723c
Arturo <arturolopeze@outlook.com>
2026-05-16 21:55:03 -0600

    Change plan

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,24 +1,7 @@
 function Hero() {
     return (
-        <section>
-            <div>
-                <h1>Arturo Lopez
-                    <span className="subtitulo">Backend Developer | IoT Systems</span>
-                    <span className="intro">I build scalable backend systems 
-                        <br />for connected devices, real-time data, and cloud infrastructure. 
-                    </span>
-                </h1>
-            </div>
-
-            <div>
-                <section>
-                    <span className="about">
-                        Hola soy Arturo, desarrollador con base en Ciudad de Mexico, 
-                        <br />experiencia en Backend con conocimientos en sistemas IoT y 
-                        <br/>enfoque hacia arquitectura Cloud escalable
-                    </span>
-                </section>
-            </div>
+        <section id="Hero">
+            
         </section>
     )
 }
